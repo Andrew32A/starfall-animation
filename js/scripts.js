2abf954bd26662f8b0819cdb0d6cac9bf27e1d12
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-12-15 00:36:39 -0800

    added floor and adjusted shatter parameters

--- js/scripts.js
+++ js/scripts.js
@@ -44,13 +44,19 @@ Star.prototype.update = function() {
     this.draw()
 
     // when the star hits the bottom of the screen, the star moves upwards
-    if (this.y + this.radius + this.velocity.y > canvas.height) {
+    if (this.y + this.radius + this.velocity.y > canvas.height - groundHeight) {
         this.velocity.y = -this.velocity.y * this.friction
         this.shatter()
     }
 
     else {
         this.velocity.y += this.gravity
+    }
+
+    // when star hits side of screen, it bounces to the opposite x coordinate
+    if (this.x + this.radius + this.velocity.x > canvas.width || this.x - this.radius <= 0) {
+        this.velocity.x = -this.velocity.x * this.friction
+        this.shatter()
     }
 
     this.x += this.velocity.x
@@ -96,7 +102,7 @@ MiniStar.prototype.update = function() {
     this.draw()
 
     // when the star hits the bottom of the screen, the star moves upwards
-    if (this.y + this.radius + this.velocity.y > canvas.height) {
+    if (this.y + this.radius + this.velocity.y > canvas.height - groundHeight) {
         this.velocity.y = -this.velocity.y * this.friction
     }
 
@@ -139,6 +145,7 @@ let miniStars
 let backgroundStars
 let ticker = 0
 let randomSpawnRate = 75
+const groundHeight = 100
 function init() {
     stars = []
     miniStars = []
@@ -168,6 +175,8 @@ function animate() {
     createMountainRange(1, canvas.height - 50, "#384551")
     createMountainRange(2, canvas.height - 100, "#2b3843")
     createMountainRange(3, canvas.height - 300, "#26333e")
+    context.fillStyle = "#182028"
+    context.fillRect(0, canvas.height - groundHeight, canvas.width, groundHeight)
 
     stars.forEach((star, index) => {
         star.update()
@@ -186,9 +195,10 @@ function animate() {
     ticker++
     
     if (ticker % randomSpawnRate == 0) {
-        const x = Math.random() * canvas.width
         // radius must be divisible by 3! else an error will occur
-        stars.push(new Star(x, -100, 12, "#e3eaef"))
+        const radius = 12
+        const x = Math.max(radius, Math.random() * canvas.width - radius)
+        stars.push(new Star(x, -100, radius, "#e3eaef"))
         randomSpawnRate = randomIntFromRange(75, 200)
     }
 }
