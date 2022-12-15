0b17c4ba6735f1b465bfc1e3e3e109a565176912
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-12-15 00:25:58 -0800

    added stars spawner and randomized starting x coordinates

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@ function Star(x, y, radius, color) {
     this.radius = radius
     this.color = color
     this.velocity = {
-        x: 0,
+        x: (Math.random() - 0.5) * 8,
         y: 3
     }
     this.friction = 0.8
@@ -53,6 +53,7 @@ Star.prototype.update = function() {
         this.velocity.y += this.gravity
     }
 
+    this.x += this.velocity.x
     this.y += this.velocity.y
 }
 
@@ -136,14 +137,16 @@ backgroundGradient.addColorStop(1, "#3f586b")
 let stars
 let miniStars
 let backgroundStars
+let ticker = 0
+let randomSpawnRate = 75
 function init() {
     stars = []
     miniStars = []
     backgroundStars = []
 
-    for (let i = 0; i < 1; i++) {
-        stars.push(new Star(canvas.width / 2, 30, 30, "#e3eaef"))
-    }
+    // for (let i = 0; i < 1; i++) {
+    //     stars.push(new Star(canvas.width / 2, 30, 30, "#e3eaef"))
+    // }
 
     for (let i = 0; i < 150; i++) {
         const x = Math.random() * canvas.width
@@ -179,6 +182,15 @@ function animate() {
             miniStars.splice(index, 1)
         }
     })
+
+    ticker++
+    
+    if (ticker % randomSpawnRate == 0) {
+        const x = Math.random() * canvas.width
+        // radius must be divisible by 3! else an error will occur
+        stars.push(new Star(x, -100, 12, "#e3eaef"))
+        randomSpawnRate = randomIntFromRange(75, 200)
+    }
 }
 
 //################################################
